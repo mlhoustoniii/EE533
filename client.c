33669edb343c54258801d5d1800b96acbe82d263
mlhoustoniii <63390065+mlhoustoniii@users.noreply.github.com>
2026-01-16 13:41:32 -0800

    Add files via upload
    Initial commit

---
+++ client.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <stdlib.h>     // exit(), atoi()
+#include <strings.h>    // bzero()
+#include <unistd.h>     // read(), write(), close(), fork()
+#include <sys/types.h>  // pid_t
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;              // file descriptors; sockfd stores value returned by socket system call
+
+    struct sockaddr_in serv_addr;       // struct for grouping variables; sockaddr_in contains an internet address
+    struct hostent *server;             // server is a pointer within hostent
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);             // converts our input string and stores port number
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);   //returns -1 if unsuccessful, otherwise it is the reference to the socket. '0' means to choose TCP protocol
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);    // returns NULL if unsuccessful
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr)); // initializes serv_addr to all zeros
+    serv_addr.sin_family = AF_INET;                // assigns the code (AF_INET) to the socket internet family (sin_family)
+    bcopy((char *)server->h_addr,                   // copies the server’s IP address from the hostent structure into the sockaddr_in structure.
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);             // converts from host byte order to network byte order (big Endian) then assigns to the sin_port
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) // establishes the connection through 'connect'; arguments are socket file descriptor, address of host, and size of the server address
+        error("ERROR connecting");
+    while(1)                                            // takes users input and transmits, reading response from server
+    {
+        printf("Please enter the message: ");
+        bzero(buffer,256);                              // clear buffer
+        fgets(buffer,255,stdin);                        // place input from user into buffer
+        n = write(sockfd,buffer,strlen(buffer));        // write buffer's contents into sockfd (file descriptor) (for transmit)
+        if (n < 0)
+            error("ERROR writing to socket");
+        bzero(buffer,256);                              // clear buffer
+        n = read(sockfd,buffer,255);                    // place contents from sockfd into buffer (for receive)
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("%s\n",buffer);                          // prints output to user
+    }
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,84 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>     // exit(), atoi()
+#include <strings.h>    // bzero()
+#include <unistd.h>     // read(), write(), close(), fork()
+#include <sys/types.h>  // pid_t
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int dostuff(int newsockfd, int cli_pid);
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno, clilen;     // sockfd for storing values from socket call, and newsockfd to accept the system call
+    struct sockaddr_in serv_addr, cli_addr;    // for server address and client address
+    int pid, cli_pid;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;            // For internet domain (sin = socket internet)
+    serv_addr.sin_addr.s_addr = INADDR_ANY;    // gets IP address of machine where the server is running
+    serv_addr.sin_port = htons(portno);        // converts from host byter ordering to network byte ordering
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,   // binds socket to an address
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");        // an example of failure could be from the socket already being in use
+    listen(sockfd,5);                          // listen on the socket for connections; 5 is maximum backlog queue of waiting connections
+    clilen = sizeof(cli_addr);
+    
+    while (1)                                   // continuously accepts new connections as they come in
+    {
+        newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        pid = fork();                               // fork (create) child process from parent
+        if (pid < 0)
+            error("ERROR on fork");
+        if (pid == 0)                               // instructions for child to exectute
+        {
+            close(sockfd);                          // child closes sockfd (listening socket)
+            cli_pid = getpid();
+            dostuff(newsockfd, cli_pid);                     // child calls dostuff
+            exit(0);                                // exits upon handling of client
+        }
+        else                                        // code for parent to execute, after calling child
+            close(newsockfd);                       // parent closes newsockfd; goes back to accept to wait for new client
+    } /* end of while */
+    return 0;
+} /*end of main*/
+    
+//     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); // when client connection established, returns new file descriptor
+//    //  signal(SIGCHLD,SIG_IGN);                    // prevents process becoming a zombie
+//     if (newsockfd < 0)
+//          error("ERROR on accept");
+    
+int dostuff(int newsockfd, int cli_pid)
+{
+    char buffer[256];
+    int n;
+    while(1)                                                // existing connection remains established and reads latest message from client
+    {
+        bzero(buffer,256);
+        n = read(newsockfd,buffer,255);                    // reads string into buffer from the socket
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\nFrom: %d\n",buffer, cli_pid);        // displays received data
+        n = write(newsockfd,"I got your message",18);      // sends auto response to client, if transmission was a success
+        if (n < 0) error("ERROR writing to socket");
+    }
+    return 0;
+}
+
